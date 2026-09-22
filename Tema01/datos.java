afe663b16b8c8fbb5c8a7dcd1a248e5f6cca3c7a
Yiognmrtz <100059682+Yiognmrtz@users.noreply.github.com>
2026-09-22 14:24:30 +0200

    datos

---
+++ Tema01/datos.java
@@ -0,0 +1,9 @@
+
+public class datos {
+    public static void main (String[] args) {
+        System.out.println(" Nombre  Direccion     Nº telefomo");
+        System.out.println("-------------------------------------");
+        System.out.printf("%-10s    %8.2s   %10d\n", "Diego", "avenida santa marta", 60102110);
+    
+    }
+}
