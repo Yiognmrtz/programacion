1b8b79c99f81341e96dc6b4741041d6e027eab09
Yiognmrtz <100059682+Yiognmrtz@users.noreply.github.com>
2026-09-24 08:34:19 +0200

    Ejercicio4
    diccionario

---
+++ Tema01/diccionario.java
@@ -0,0 +1,17 @@
+public class diccionario{
+    public static void main (String[] args) {
+System.out.println("ingles\tespañol");
+System.out.println("======\t=====");
+System.out.println("house\tcasa");
+System.out.println("dog\thouse");
+System.out.println("cat\tgato");
+System.out.println("home\thogar");
+System.out.println("Car\tcoche");
+System.out.println("women\tmujer");
+System.out.println("Bird\tpajaro");
+System.out.println("brother\thermano");
+System.out.println("sister\thermana");
+System.out.println("Dad\tpadre");
+    
+    }
+}
