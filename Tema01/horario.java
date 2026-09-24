ea7dbcd68b2cd70c51fcf1fd94b236f9bf66cccc
Yiognmrtz <100059682+Yiognmrtz@users.noreply.github.com>
2026-09-24 08:57:14 +0200

    Ejercicio5
    horario de clases

---
+++ Tema01/horario.java
@@ -0,0 +1,15 @@
+public class horario {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hora\tLunes\t\tMartes\t\tMiércoles\tJueves\t\tViernes");
+        System.out.println("-------------------------------------------------------------------------------------------");
+
+        System.out.println("8:00\tLenguaje\tBases de datos\tSistemas\tProgramación\tEntornos");
+        System.out.println("9:00\tLenguaje\tBases de datos\tSistemas\tProgramación\tBases de datos");
+        System.out.println("10:00\tSistemas\tSostenibilidad\tIpe        \tLenguaje      \tBases de datos");
+        System.out.println("11:00\tSistemas\tEntornos\tIpe          \tLenguaje      \tIpe");
+        System.out.println("12:00\tprogramacion\tprogramacion\tDigitalizacion\tbases de datos\tProgramacion");
+        System.out.println("13:00\tprogramacion\tprogramacion\tEntornos\tbases de datos\tProgramacion");
+    }
+}
